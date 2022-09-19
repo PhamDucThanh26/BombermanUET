003a3d0c03f7599b696a808e6ba0bb2461fbabe3
nhandt2101 <21020371@vnu.edu.vn>
2022-09-19 21:37:32 +0700

    Comment Main

--- src/uet/oop/bomberman/Main.java
+++ src/uet/oop/bomberman/Main.java
@@ -3,6 +3,7 @@ package uet.oop.bomberman;
 import javafx.application.Application;
 
 public class Main {
+    //Bai tap lon UET.
     public static void main(String[] args) {
         Application.launch(BombermanGame.class);
     }
