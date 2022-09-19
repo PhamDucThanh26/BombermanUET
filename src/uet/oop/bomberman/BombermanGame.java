0e461559b0fbf4fa90ef55cacacaa9bd9a6dbefe
Doan Nguyen <21020551@vnu.edu.vn>
2022-09-19 18:49:27 +0700

    sheesh

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -27,9 +27,9 @@ public class BombermanGame extends Application {
     private List<Entity> stillObjects = new ArrayList<>();
 
 
-    public static void main(String[] args) {
-        Application.launch(BombermanGame.class);
-    }
+//    public static void main(String[] args) {
+//        Application.launch(BombermanGame.class);
+//    }
 
     @Override
     public void start(Stage stage) {
