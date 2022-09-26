2f1f1525fe8cbe62c221cf56b8e1beb7847f79f2
Doan Nguyen <21020551@vnu.edu.vn>
2022-09-26 23:33:16 +0700

    update bomber sprites

--- src/uet/oop/bomberman/entities/Bomber.java
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -9,8 +9,37 @@ import javafx.scene.paint.Color;
 import uet.oop.bomberman.user_input.Keyboard;
 
 public class Bomber extends Entity {
-    private int xVec = 0;
-    private int yVec = 0;
+    int xVec = 0;
+    int yVec = 0;
+
+    final int FPS = 60;
+    int frameRate = 0;
+    int frameCount = 0;
+    private boolean moving = false;
+
+    Image[] upAnimation = {
+            Sprite.player_up.getFxImage(),
+            Sprite.player_up_1.getFxImage(),
+            Sprite.player_up_2.getFxImage()
+    };
+
+    Image[] downAnimation = {
+            Sprite.player_down.getFxImage(),
+            Sprite.player_down_1.getFxImage(),
+            Sprite.player_down_2.getFxImage()
+    };
+
+    Image[] leftAnimation = {
+            Sprite.player_left.getFxImage(),
+            Sprite.player_left_1.getFxImage(),
+            Sprite.player_left_2.getFxImage()
+    };
+
+    Image[] rightAnimation = {
+            Sprite.player_right.getFxImage(),
+            Sprite.player_right_1.getFxImage(),
+            Sprite.player_right_2.getFxImage()
+    };
 
     public Bomber(int x, int y, Image img) {
         super( x, y, img);
@@ -18,6 +47,51 @@ public class Bomber extends Entity {
 
     @Override
     public void update() {
+
+    }
+
+    public void update(Keyboard a) {
+        updateMove(a);
+        move();
+        updateAnimation();
+    }
+
+    public void updateMove(Keyboard a) {
+        xVec = 0;
+        yVec = 0;
+        if(a.up) {
+            yVec -= 2;
+            this.setImg(upAnimation[frameCount]);
+        }
+        if(a.down) {
+            yVec += 2;
+            moving = true;
+            this.setImg(downAnimation[frameCount]);
+        }
+        if(a.left) {
+            xVec -= 2;
+            moving = true;
+            this.setImg(leftAnimation[frameCount]);
+        }
+        if(a.right)  {
+            xVec += 2;
+            moving = true;
+            this.setImg(rightAnimation[frameCount]);
+        }
+        if(a.plant_bomb) {
+        }
+    }
+
+    private void updateAnimation() {
+        frameRate++;
+        if(FPS / frameRate == 4) {
+            frameRate = 0;
+            frameCount++;
+            frameCount %= 3;
+        }
+    }
+
+    private void move() {
         x += xVec;
         y += yVec;
 
@@ -33,10 +107,10 @@ public class Bomber extends Entity {
     public void updateMove(Keyboard a) {
         xVec = 0;
         yVec = 0;
-        if(a.up) yVec -= 1;
-        if(a.down) yVec += 1;
-        if(a.left) xVec -= 1;
-        if(a.right) xVec += 1;
+        if(a.up) yVec -= 3;
+        if(a.down) yVec += 3;
+        if(a.left) xVec -= 3;
+        if(a.right) xVec += 3;
         if(a.plant_bomb) {
 
         }
