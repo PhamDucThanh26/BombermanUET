70656e331e3256cf96f1b74a57591421f1fe75ad
Doan Nguyen <21020551@vnu.edu.vn>
2022-10-06 14:58:16 +0700

    update creature

--- src/uet/oop/bomberman/entities/Oneal.java
+++ src/uet/oop/bomberman/entities/Oneal.java
@@ -2,10 +2,12 @@ package uet.oop.bomberman.entities;
 
 import javafx.scene.image.Image;
 
-public class Oneal extends Entity {
+public class Oneal extends Creature {
     public Oneal(int xUnit, int yUnit, Image img) {
         super(xUnit, yUnit, img);
     }
+
+
 
     @Override
     public void update() {
