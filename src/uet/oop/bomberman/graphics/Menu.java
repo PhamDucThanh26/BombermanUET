bdbd7b0b4c5afd8fb469c8eb76843925fad1f151
nhandt2101 <21020371@vnu.edu.vn>
2022-10-22 08:54:14 +0700

    add Menu

--- src/uet/oop/bomberman/graphics/Menu.java
+++ src/uet/oop/bomberman/graphics/Menu.java
@@ -1,4 +1,5 @@
 package uet.oop.bomberman.graphics;
+
 import uet.oop.bomberman.SoundEffect.Sound;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
@@ -19,20 +20,20 @@ import static uet.oop.bomberman.graphics.Sprite.WIDTH;
 import static uet.oop.bomberman.level.Game.bomberman;
 
 public class Menu {
-//    public static Sound startStage = new Sound();
+    //    public static Sound startStage = new Sound();
     public static Sound backGround = new Sound();
     public static Image authorImage;
     public static ImageView author;
     private static Text newGameText, exitText, optionsText;
 
-    private static Button[] menuButton  = new Button[3];
+    private static Button[] menuButton = new Button[3];
     private static Text item[] = new Text[3];
     private static Rectangle rect[] = new Rectangle[3];
     public static Pane layoutMenu;
 
 
     public static void createMenu(Group root) {
-        for(int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             menuButton[i] = new Button();
         }
         authorImage = new Image("images/menu.png");
@@ -42,43 +43,39 @@ public class Menu {
         author.setX(0);
         author.setY(0);
 
-        for(int i = 0;i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             item[i] = new Text();
-            if(i == 0) {
+            if (i == 0) {
                 item[i].setText("New Game");
                 item[i].setX(325);
                 item[i].setY(280);
-            }
-            else if(i == 1) {
+            } else if (i == 1) {
                 item[i].setText("Options");
                 item[i].setX(340);
                 item[i].setY(340);
-            }
-            else {
+            } else {
                 item[i].setText("Exit");
                 item[i].setX(360);
                 item[i].setY(400);
             }
-            item[i].setFont(Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR,25));
+            item[i].setFont(Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 25));
             item[i].setFill(Color.WHITE);
             item[i].setStroke(Color.BLACK);
         }
 
-        for(int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             rect[i] = new Rectangle();
 
-            if(i == 0) {
+            if (i == 0) {
                 rect[i].setY(250);
-            }
-            else if(i == 1) {
+            } else if (i == 1) {
                 rect[i].setY(310);
-            }
-            else {
+            } else {
                 rect[i].setY(370);
             }
             rect[i].setWidth(200);
             rect[i].setHeight(40);
-            rect[i].setX(WIDTH/2-rect[i].getWidth()/2);
+            rect[i].setX(WIDTH / 2 - rect[i].getWidth() / 2);
             rect[i].setFill(Color.ORANGE);
             rect[i].setArcHeight(30);
             rect[i].setArcWidth(30);
@@ -86,11 +83,11 @@ public class Menu {
             rect[i].setStrokeWidth(3);
         }
         layoutMenu = new Pane();
-        layoutMenu.getChildren().addAll(rect[0], rect[1],rect[2],item[0],item[1],item[2]);
-        layoutMenu.setMinSize(600,600);
-        layoutMenu.setMaxSize(600,600);
-        root.getChildren().addAll(author,layoutMenu);
-        for(Text i : item) {
+        layoutMenu.getChildren().addAll(rect[0], rect[1], rect[2], item[0], item[1], item[2]);
+        layoutMenu.setMinSize(600, 600);
+        layoutMenu.setMaxSize(600, 600);
+        root.getChildren().addAll(author, layoutMenu);
+        for (Text i : item) {
             i.setOnMouseEntered(event -> {
                 i.setFont(Font.font("BOMBERMA", FontWeight.BOLD, FontPosture.REGULAR, 25));
                 i.setFill(Color.RED);
@@ -110,7 +107,7 @@ public class Menu {
             layoutMenu.setTranslateY(-1000);
             backGround.playBackGround();
         });
-        item[2].setOnMouseClicked(event->{
+        item[2].setOnMouseClicked(event -> {
             stage.close();
         });
     }
